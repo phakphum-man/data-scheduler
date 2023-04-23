5ce7d4031dcf744e0da372c24983b2a9d9635454
Phakphum Kunakong <icedev001@gmail.com>
2023-04-23 19:01:12 +0700

    add first job

--- endpoints/index.js
+++ endpoints/index.js
@@ -1,8 +1,22 @@
+const axios = require('axios');
 module.exports = function (app) {
 	
     app.get('/', (req, res) => {
         // #swagger.ignore = true
 
         return res.status(200).send(`Start API Scheduler`);
+    });
+
+    app.post('/everyThirtyMinute', (req, res) => {
+        // #swagger.ignore = true
+
+        let jobUrl = "https://data-keeper.onrender.com/livinginsider/chonburi";
+        axios.get(jobUrl)
+        .then( oxioRes => {
+            console.log(`${jobUrl} => ${oxioRes.data}`)
+        })
+        .catch(err => console.error(err));
+
+        return res.status(200).send({message: "success"});
     });
 };
