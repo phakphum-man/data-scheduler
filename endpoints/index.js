e1905a01315c0e46384e8f7a059d594105428950
Phakphum Kunakong <icedev001@gmail.com>
2023-04-23 21:55:44 +0700

    add table schedules

--- endpoints/index.js
+++ endpoints/index.js
@@ -1,4 +1,10 @@
 const axios = require('axios');
+const timediff = require('timediff');
+
+let activeTime = (timeTable, timeCurrent) => {
+    const diff = timediff(timeTable, timeCurrent, 'YMDHmS');
+    return (diff.hours === 0 && diff.minutes === 0 && diff.seconds === 0)
+};
 module.exports = function (app) {
 	
     app.get('/', (req, res) => {
@@ -7,15 +13,42 @@ module.exports = function (app) {
         return res.status(200).send(`Start API Scheduler`);
     });
 
-    app.post('/everyThirtyMinute', (req, res) => {
+    app.get('/everyThirtyMinute', (req, res) => {
         // #swagger.ignore = true
+        const tables = [{
+            runtime: "1998-01-01 07:30:00",
+            url: "https://data-keeper.onrender.com/livinginsider/chonburi",
+            param: null
+        },{
+            runtime: "1998-01-01 08:30:00",
+            url: "https://data-keeper.onrender.com/livinginsider/rayong",
+            param: null
+        },{
+            runtime: "1998-01-01 09:30:00",
+            url: "https://data-keeper.onrender.com/livinginsider/sellcost",
+            param: null
+        }];
 
-        let jobUrl = "https://data-keeper.onrender.com/livinginsider/chonburi";
-        axios.get(jobUrl)
-        .then( oxioRes => {
-            console.log(`${jobUrl} => ${oxioRes.data}`)
-        })
-        .catch(err => console.error(err));
+        var datetime = new Date();
+        let schedules = tables.map(tb => ({
+            runtime: tb.runtime,
+            url: tb.url,
+            param: tb.param,
+            isTrigger: activeTime(tb.runtime, datetime)
+        }));
+
+        const runtimes = schedules.filter((tb)=> tb.isTrigger);
+        console.log(runtimes.length);
+        runtimes.forEach((j) => {
+
+            let jobUrl = j.url;
+            axios.get(jobUrl)
+            .then( oxioRes => {
+                console.log(`${jobUrl} => ${oxioRes.data}`)
+            })
+            .catch(err => console.error(err));
+
+        });
 
         return res.status(200).send({message: "success"});
     });
