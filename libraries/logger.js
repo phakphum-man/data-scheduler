316816c2badf0c0f3f96ea4946b49b8c0c2eecb9
Phakphum Kunakong <icedev001@gmail.com>
2023-05-07 23:07:49 +0700

    update to winston-s3-transport

--- libraries/logger.js
+++ libraries/logger.js
@@ -1,7 +1,7 @@
 'use strict';
-
+require('dotenv').config();
 const { createLogger, format, transports } = require("winston");
-const s3 = require('../libraries/s3');
+const S3Transport = require("winston-s3-transport");
 
 const env = process.env.NODE_ENV || 'development';
 
@@ -9,7 +9,27 @@ const timezoned = () => {
   return new Date().toLocaleString('en-US', {
       timeZone: process.env.TZ
   });
-}
+};
+
+const s3Transport = new S3Transport({
+  s3ClientConfig: {
+    region: process.env.AWS_REGION,
+  },
+  s3TransportConfig: {
+    bucket: process.env.CYCLIC_BUCKET_NAME,
+    group: (logInfo) => {
+      // Group logs with `userId` value and store them in memory. 
+      // If the 'userId' value does not exist, use the `anonymous` group.
+      return logInfo?.message?.userId || "anonymous";
+    },
+    bucketPath: () => {
+      // The bucket path in which the log is uploaded. 
+      // You can create a bucket path by combining `groupId`, `timestamp`, and `uuid` values.
+      return `logs/filelogger.log`;
+    },
+  },
+});
+
 const logger = createLogger({
     // change level if in dev environment versus production
     level: env === 'development' ? 'debug' : 'info',
@@ -28,12 +48,9 @@ const logger = createLogger({
               info => `${info.timestamp} ${info.level}: ${info.message}`
             )
           )
-        })
+        }),
+        s3Transport
       ]
 });
-
-logger.on('data', (chunk => { // log listener
-  s3.write(JSON.stringify(chunk)) // call s3 uploader and pass stringyfied json log
-}));
 
 module.exports = logger;
