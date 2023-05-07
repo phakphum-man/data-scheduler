067b99439adaab7c00253a146f8689cf9caf244d
Phakphum Kunakong <icedev001@gmail.com>
2023-05-07 22:32:25 +0700

    fix get s3 file

--- endpoints/logs.js
+++ endpoints/logs.js
@@ -8,12 +8,13 @@ module.exports = function (app) {
         // #swagger.description = 'Endpoint download file.'
         // #swagger.parameters['name'] = { type: 'string', description: 'name is required.' }
 
-        const contentfile = await s3.getlog();
+        const s3File = await s3.getlog();
 
         /* #swagger.responses[200] = { 
                description: 'File Content.' 
         } */
-        return res.status(200).send(contentfile);
+        res.set('Content-type', s3File.ContentType)
+        res.send(s3File.Body.toString()).end();
     });
 
     app.delete('/log', async (req, res) => {
