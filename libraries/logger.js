e0074bc56f53756976800bd0b2a1eb5ada3b3c93
Phakphum Kunakong <icedev001@gmail.com>
2023-05-07 23:15:51 +0700

    fix to stream

--- libraries/logger.js
+++ libraries/logger.js
@@ -1,7 +1,7 @@
 'use strict';
 require('dotenv').config();
 const { createLogger, format, transports } = require("winston");
-const S3Transport = require("winston-s3-transport");
+const { S3StreamLogger } = require('s3-streamlogger');
 
 const env = process.env.NODE_ENV || 'development';
 
@@ -11,23 +11,13 @@ const timezoned = () => {
   });
 };
 
-const s3Transport = new S3Transport({
-  s3ClientConfig: {
-    region: process.env.AWS_REGION,
-  },
-  s3TransportConfig: {
-    bucket: process.env.CYCLIC_BUCKET_NAME,
-    group: (logInfo) => {
-      // Group logs with `userId` value and store them in memory. 
-      // If the 'userId' value does not exist, use the `anonymous` group.
-      return logInfo?.message?.userId || "anonymous";
-    },
-    bucketPath: () => {
-      // The bucket path in which the log is uploaded. 
-      // You can create a bucket path by combining `groupId`, `timestamp`, and `uuid` values.
-      return `logs/filelogger.log`;
-    },
-  },
+const s3_stream = new S3StreamLogger({
+  Bucket: process.env.CYCLIC_BUCKET_NAME,
+  Key: 'logs/filelogger.log'
+});
+
+const s3Transport = new (transports.Stream)({
+  stream: s3_stream
 });
 
 const logger = createLogger({
