314746e085cbe8df6f65fd81e6e491dc5b3927a1
Phakphum Kunakong <icedev001@gmail.com>
2023-05-09 11:37:49 +0700

    update schedule

--- endpoints/index.js
+++ endpoints/index.js
@@ -23,15 +23,15 @@ module.exports = function (app) {
         // #swagger.ignore = true
         const iv = process.env.IV || await fs.promises.readFile(path.join(process.cwd(), "iv.txt"), 'utf8');
         const tables = [{
-            runtime: "1998-01-01 15:30:00",//"1998-01-01 07:30:00",
+            runtime: "1998-01-01 12:30:00",//"1998-01-01 07:30:00",
             url: `${process.env.API_KEEPER}/livinginsider/chonburi?iv=${iv}`,
             param: null
         },{
-            runtime: "1998-01-01 16:30:00",//"1998-01-01 09:30:00",
+            runtime: "1998-01-01 13:30:00",//"1998-01-01 09:30:00",
             url: `${process.env.API_KEEPER}/livinginsider/rayong?iv=${iv}`,
             param: null
         },{
-            runtime: "1998-01-01 17:00:00",//"1998-01-01 09:00:00",
+            runtime: "1998-01-01 14:00:00",//"1998-01-01 09:00:00",
             url: `${process.env.API_KEEPER}/livinginsider/sellcost?iv=${iv}`,
             param: null
         }];
