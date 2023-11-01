7fe70e0c4db4bfe750cde9bb7e083d2d5764e4b2
Phakphum Kunakong <icedev001@gmail.com>
2023-11-01 15:05:38 +0700

    fix error timezone

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const moment = require('moment');
+const moment = require('moment-timezone');
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
